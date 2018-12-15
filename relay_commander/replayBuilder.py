fe52085603a38756cb6ecea0fd7af244eab5f028
Manuel Partida <manuel@launchdarkly.com>
2018-12-14 18:27:30 -0800

    add comments to validator + replayBuilder (refactored)

--- relay_commander/replayBuilder.py
+++ relay_commander/replayBuilder.py
@@ -1,47 +1,44 @@
-from subprocess import run
-
 import os
 import uuid
-
-
-def createDirectories(directory):
-    if not os.path.exists(directory):
-        os.makedirs(directory)
+from subprocess import run
 
 def checkLocal():
-    toCheck = ['./replay', './replay/toDo', './replay/archive']
-    
-    for i in toCheck:
-        createDirectories(i)
+	"""Check for directories and create if not already there
+	"""
+	toCheck = ['./replay', './replay/toDo', './replay/archive']
+	
+	for i in toCheck:
+		if not os.path.exists(i):
+			os.makedirs(i)
 
 def createFile(project, environment, feature, state):
-    checkLocal()
-    savePath = './replay/toDo/'
-    filename = str(uuid.uuid1()) + '.txt'
-    completeName = os.path.join(savePath, filename)
+	"""Create file with RC command that will be called against LaunchDarkly API.
 
-    with open(completeName, 'w') as file_object:
-        file_object.write('rc ld-api -p ' + project + ' -e ' + environment + ' -f ' + feature + ' -s ' + state)
+	:param Project: LaunchDarkly Project
+	:param environment: LaunchDarkly Environment
+	:param feature: LaunchDarkly Feature
+	:param state: State to update feature flag
+	"""
+	checkLocal()
+	savePath = './replay/toDo/'
+	filename = '{0}.txt'.format(str(uuid.uuid1()))
+	completeName = os.path.join(savePath, filename)
+	
+	with open(completeName, 'w') as file_object:
+		file_object.write('rc update-ld-api -p {0} -e {1} -f {2} -s {3}'.format(project, environment, feature, state)
 
 def executeReplay():
-    # get list of files in directory
-    path = './replay/toDo/'
-
-    # order from oldest to newest
-    getFiles = os.listdir(path)
-    getFiles = [os.path.join(path, file) for file in getFiles]
-    sortFiles = sorted(getFiles, key=os.path.getctime)
-
-    # iterate through list of files
-    for i in sortFiles:
-        getFile = open(i)
-
-        runUpdate = getFile.read()
-    # once file is accessed, execute the command within their
-        run(runUpdate, shell=True)
-
-    # after executed, move the file to archive directory
-        getFile.close()
-        moveFile = 'mv ' + i + ' ./replay/archive/'
-        run(moveFile, shell=True)
-
+	"""Will itereate through files created in ./replay/toDO and move them to archive.
+	"""
+	path = './replay/toDo/'
+	getFiles = os.listdir(path)
+	getFiles = [os.path.join(path, file) for file in getFiles]
+	sortFiles = sorted(getFiles, key=os.path.getctime)
+	
+	for i in sortFiles:
+		getFile = open(i)
+		runUpdate = getFile.read()
+		run(runUpdate, shell=True)
+		getFile.close()
+		moveFile = 'mv {0} ./replay/archive/'.format(i)
+		run(moveFile, shell=True)
--- relay_commander/validator.py
+++ relay_commander/validator.py
@@ -1,7 +1,10 @@
 def validateState(state):
-    
-    lowerCaseState = state.lower()
+    """Checks that either true or false was entered and make it lower case
 
+    :param state: State to validate
+    """
+    lowerCaseState = state.lower()
+    
     if lowerCaseState == 'true' or lowerCaseState == 'false':
         return True
     else:
