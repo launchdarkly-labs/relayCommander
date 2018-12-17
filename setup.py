8105f5d249e9f8ecbd57d2a12a9198ecc7f69678
Lev Lazinskiy <lev@launchdarkly.com>
2018-12-17 11:27:48 -0800

    release 0.0.4

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup
 from setuptools.command.install import install
 
 # relayCommander version
-VERSION = "0.0.3"
+VERSION = "0.0.4"
 
 def readme():
     """print long description"""
