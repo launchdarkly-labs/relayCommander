f713b118cb2be707ed2fa368321e3d4fedb67ec7
Lev Lazinskiy <lev@levlaz.org>
2019-03-09 18:10:32 -0800

    refactor validation tests

--- tests/relay_commander/testValidator.py
+++ tests/relay_commander/testValidator.py
@@ -1,24 +1,24 @@
 import unittest
 
-from relay_commander.validator import validateState
+from relay_commander.validator import valid_state, valid_env_vars
 
 
 class TestValidator(unittest.TestCase):
 
     def testInvalidInput(self):
-        result = validateState('bad')
+        result = valid_state('bad')
         self.assertFalse(result)
-    
+
     def testValidInput(self):
-        result = validateState('on')
+        result = valid_state('on')
         self.assertTrue(result)
 
-        result = validateState('off')
+        result = valid_state('off')
         self.assertTrue(result)
-    
+
     def testValidInputMixcase(self):
-        result = validateState('oN')
+        result = valid_state('oN')
         self.assertTrue(result)
 
-        result = validateState('OfF')
+        result = valid_state('OfF')
         self.assertTrue(result)
